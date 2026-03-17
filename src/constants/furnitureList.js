335892d6ba35044180c78df478391e6e5202baee
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-03-17 14:49:48 -0300

    Añadiendo imagenes y estructura general de la app

---
+++ src/constants/furnitureList.js
@@ -0,0 +1,14 @@
+export const furnitureList = [
+    {id: 0, name: "Biblioteca con puertas bajas", alt:"Imagen de biblioteca con puertas bajas", img: "/src/assets/furniture/biblioteca-puertas-bajas-oficina.webp", price: "708500"},
+    {id: 1, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 2, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 3, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 4, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 5, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 6, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 7, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 8, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 9, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 10, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+    {id: 11, name: "Cajonera de 4 cajones", alt:"Imagen de cajonera con 4 cajones", img: "/src/assets/furniture/cajonera-dormitorio.webp", price: "425000"},
+]
